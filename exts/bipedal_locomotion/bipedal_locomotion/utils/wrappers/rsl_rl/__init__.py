6a75f7b62228f43f47651cf261d3e0eedca511f4
Xiong Hongwei <xxxionghw@gmail.com>
2024-12-30 14:55:17 +0800

    update rsl_rl wrappers

---
+++ exts/bipedal_locomotion/bipedal_locomotion/utils/wrappers/rsl_rl/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Wrappers and utilities to configure an :class:`ManagerBasedRLEnv` for RSL-RL library."""
+
+from omni.isaac.lab_tasks.utils.wrappers.rsl_rl import *
+
+from .rl_mlp_cfg import RslRlOnPolicyRunnerMlpCfg, RslRlPpoAlgorithmMlpCfg, MlpModuleCfg
